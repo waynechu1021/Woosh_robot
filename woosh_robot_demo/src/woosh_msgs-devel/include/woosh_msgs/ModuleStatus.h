199e308bc676d3ad4aa06d7c9fed61e53e009e7a
wayne <1149740188@qq.com>
2025-02-21 10:42:06 +0800

    rm msg file

--- woosh_robot_demo/src/woosh_msgs-devel/include/woosh_msgs/ModuleStatus.h
+++
@@ -1,9 +0,0 @@
-#include "woosh_msgs/ModuleStatusElec.h"
-#include "woosh_msgs/ModuleStatusEmbed.h"
-#include "woosh_msgs/ModuleStatusSys.h"
-#include "woosh_msgs/ModuleStatusApp.h"
-
-using namespace elec;
-using namespace embed;
-using namespace sys;
-using namespace app;
--- woosh_robot_demo/src/woosh_msgs-devel/map_server/revise.md
+++
@@ -1,1 +0,0 @@
-NavMode.msg中 in是被保留的关键字 因此修改成nav_in，out也一样
