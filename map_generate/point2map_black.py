d777d333c3125826cac706d77223fd24e9ddfa0f
Benaidandan <99269661+Benaidandan@users.noreply.github.com>
2025-06-17 16:40:22 +0800

    Update point2map_black.py
    两次投影

--- map_generate/point2map_black.py
+++ map_generate/point2map_black.py
@@ -3,27 +3,32 @@ import numpy as np
 import cv2
 import yaml
 
+"""
+
+功能:将点云数据转换为2D占用栅格地图(高度为z轴,点云一般比较杂乱，且可能不是封闭的)
+主要步骤：
+1. 读取点云数据
+2. 采样和过滤孤立点
+3. 生成2D占用栅格地图
+4. 所有点云投影到黑色幕布上显示成白色（所有区域）
+5. 过滤出障碍区域的点，继续投影显示黑色
+6. 处理小区域（去除噪点）
+7. 保存为ROS格式的地图文件
+"""
+
 # 配置参数
-file_directory = "map_generate/"  
-file_name = "record_2_13_raw_pre_o3d"
+file_directory = "/map_generate/"  
+file_name = "cloud-万科里"
 pcd_file = file_directory + file_name + ".ply"
+
+
 map_resolution = 0.05
-thre_z_min =  1.77139329 + 0.15#保留点云的最低高度:readpoint_height[0] +0.15获得
-thre_z_max =  3.3#保留点云的最高高度，单位m
-thre_radius = 0.05 #半径滤波 单位m
-thres_point_count = 6 #半径滤波的个数 
-min_area_count = 10 #去除黑色小点点的最小黑色面积
-# 半径滤波：去除周围邻域内点数少的点
-def radius_outlier_filter(cloud, radius, min_neighbors):
-    tree = o3d.geometry.KDTreeFlann(cloud)
-    points = np.asarray(cloud.points)
-    mask = np.ones(len(points), dtype=bool)
-    
-    for i, point in enumerate(points):
-        [k, idx, _] = tree.search_radius_vector_3d(point, radius)
-        if len(idx) < min_neighbors:
-            mask[i] = False
-    return cloud.select_by_index(np.where(mask)[0])
+thre_z_min =  0.2#保留点云的最低高度:readpoint_height[0] +0.15获得
+thre_z_max =  1.8#保留点云的最高高度，单位m
+thre_radius = 0.1 #半径滤波 单位m
+thres_point_count = 3 #半径滤波的个数 
+min_area_count = 20 #去除黑色小点点的最小黑色面积
+
 
 
 def remove_black_regions(A, min_area=min_area_count):
@@ -54,13 +59,39 @@ def remove_black_regions(A, min_area=min_area_count):
     return processed_image
 
 
+def remove_white_regions(A, min_area=min_area_count):
+    """
+    处理矩阵 A:去除小的白色区域。
+    参数:
+        A: 输入的二值矩阵，假设白色为前景，黑色为背景
+        min_area: 最小面积阈值，用于去除小区域
+        
+    返回:
+        processed_image: 处理后的图像
+    """
+    # 反转颜色（确保黑色是前景）
+    binary = A.copy()
+
+    # 计算连通域并去除小的黑色区域
+    num_labels, labels, stats, _ = cv2.connectedComponentsWithStats(binary, connectivity=8)
+
+    # 遍历连通区域，去除小面积的黑色区域
+    for i in range(1, num_labels):  # 从1开始，跳过背景
+        if stats[i, cv2.CC_STAT_AREA] < min_area:
+            binary[labels == i] = 255  # 将小区域置为白色
+    
+    return binary
+
 # 读取PCD文件
 pcd = o3d.io.read_point_cloud(pcd_file)
+
+
+pcd = pcd.voxel_down_sample(voxel_size=0.01)  
+pcd, _ = pcd.remove_radius_outlier(nb_points=5, radius=0.1) 
 points = np.asarray(pcd.points)
 
-
-points = points[(points[:, 2] >= thre_z_min-0.3) & (points[:, 2] <= thre_z_max)]
-
+# points = points[(points[:, 2] >= thre_z_min-0.5) & (points[:, 2] <= thre_z_max + 2)]
+# print(points.shape)
 
 
 # 创建Open3D点云对象
@@ -68,7 +99,8 @@ cloud_after_filter = o3d.geometry.PointCloud()
 cloud_after_filter.points = o3d.utility.Vector3dVector(points)
 # o3d.visualization.draw_geometries([cloud_after_filter], window_name='black')
 #半径滤波
-cloud_after_filter = radius_outlier_filter(cloud_after_filter, thre_radius, thres_point_count)
+# print(len(cloud_after_filter.points))
+# cloud_after_filter = radius_outlier_filter(cloud_after_filter, thre_radius, thres_point_count)
 points_after_filter = np.asarray(cloud_after_filter.points)
 
 # 获取点云的最小最大值
@@ -78,9 +110,9 @@ x_max, y_max = np.max(points_after_filter[:, :2], axis=0)
 # 计算栅格地图大小
 map_width = int((x_max - x_min) / map_resolution)
 map_height = int((y_max - y_min) / map_resolution)
-
 # 初始化栅格地图
 occupancy_grid = np.zeros((map_height, map_width), dtype=np.uint8)
+
 for point in points_after_filter:
     x, y = point[:2]
     # 转换为栅格坐标
@@ -91,12 +123,13 @@ for point in points_after_filter:
         occupancy_grid[j, i] = 255  # 设置为区域底色：白色（255）
 
 
-# map_image_ros = np.flipud(occupancy_grid)  # 反转行
-# map_image_bgr = cv2.cvtColor(map_image_ros, cv2.COLOR_GRAY2BGR)
-# # 保存为 .png 文件
-# cv2.imwrite(file_directory + file_name + "0.png", map_image_bgr)
-
+# # map_image_ros = np.flipud(occupancy_grid)  # 反转行
+# # map_image_bgr = cv2.cvtColor(map_image_ros, cv2.COLOR_GRAY2BGR)
+# # # 保存为 .png 文件
+# # cv2.imwrite(file_directory + file_name + "0.png", map_image_bgr)
 occupancy_grid = remove_black_regions(occupancy_grid)
+# print(occupancy_grid.shape)
+
 # cv2.imwrite(file_directory + file_name + "1.png", np.flipud(occupancy_grid))
 ##
 # 二次处理：白色区域加上障碍区域
@@ -109,6 +142,10 @@ for point in points_after_filter:
     j = int((y - y_min) / map_resolution)
     if 0 <= i < map_width and 0 <= j < map_height:
         occupancy_grid[j, i] = 0  # 黑色
+occupancy_grid = remove_white_regions(occupancy_grid)
+
+# cv2.imwrite(file_directory + file_name + "2.png", np.flipud(occupancy_grid))
+
 # 翻转y轴（ros左下角为原点，opencv左上角为原点）
 map_image_ros = np.flipud(occupancy_grid)  # 反转行
 map_image_bgr = cv2.cvtColor(map_image_ros, cv2.COLOR_GRAY2BGR)
